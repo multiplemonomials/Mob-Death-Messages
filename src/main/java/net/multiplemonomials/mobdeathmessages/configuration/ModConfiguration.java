9cd61ff9da3569ba63afc851a0df8288cf3c9198
multiplemonomials <jsmith@crackofdawn.onmicrosoft.com>
2015-07-01 22:15:02 -0700

    Fix config loading weirdness.

--- src/main/java/net/multiplemonomials/mobdeathmessages/configuration/ModConfiguration.java
+++ src/main/java/net/multiplemonomials/mobdeathmessages/configuration/ModConfiguration.java
@@ -42,35 +42,35 @@ public class ModConfiguration
 
     public static void init(File configFile)
     {
-        configuration = new Configuration(configFile);
-        
-    	LogHelper.info("Loading configuration...");
-    	
-    	showPlayerOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showPlayerOnMobDeathMessages", true, "Show cases where a mob was killed by a player").getBoolean();
-    	showMobOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showMobOnMobDeathMessages", true, "Show cases where a mob was killed by a mob").getBoolean();
-    	showNamedMobsOnly = configuration.get(CATEGORY_DEATH_MESSAGES, "showNamedMobsOnly", false, "Only show death messages involving mobs which have a custom name. If MobvMob messages are enabled, the named mob can be the attacker as well.").getBoolean();
-    	showInanimateObjectOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showInanimateObjectOnMobDeathMessages", true, "Show cases where a mob was killed by an inanimate object").getBoolean();
-    	showBatsBurningToDeath = configuration.get(CATEGORY_DEATH_MESSAGES, "showBatsBurningToDeath", false, "Show bats burning to death").getBoolean();
-
-    	xpForKillingSpree = configuration.get(CATEGORY_KILLING_SPREES, "xpForKillingSpree", 10, "Amount of XP awarded to players who get a killing spree.  Each subsequent level of spree gets twice as much xp.  Set to 0 to disable.").getInt();
-    	killingSpreePlayersEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreePlayersEnabled", true, "Show player killing sprees").getBoolean();
-    	killsForKillingSpree = configuration.get(CATEGORY_KILLING_SPREES, "killsPerBaseKillingSpree", 8, "Kills to get a level one killing spree.  Subsequent levels take twice as many per level.").getInt();
-    	
-    	//initialize the rest of the values using math
-    	killsForKillingMachine = (2 * killsForKillingSpree) + killsForKillingSpree;
-    	killsForLegendary = (4 * killsForKillingSpree) + killsForKillingMachine;
-    	killsForAwesome = (8 * killsForKillingSpree) + killsForLegendary;
-    	
-    	killingSpreePlayersVsMobsEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreePlayersVsMobsEnabled", true, "Show killing sprees when mobs kill players").getBoolean();
-    	killingSpreeMobsVsMobsEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreeMobsVsMobsEnabled", true, "Show killing sprees when mobs kill mobs").getBoolean();
-    	
+   	
         try
         {
-            configuration.load();
+        	//Configurations load themselves when constructed.  Everyone else seems to manually call load() after creating one,
+        	//which I claim is unnecessary.
+        	LogHelper.info("Loading configuration...");
+            configuration = new Configuration(configFile);
+            
+        	showPlayerOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showPlayerOnMobDeathMessages", true, "Show cases where a mob was killed by a player").getBoolean();
+        	showMobOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showMobOnMobDeathMessages", true, "Show cases where a mob was killed by a mob").getBoolean();
+        	showNamedMobsOnly = configuration.get(CATEGORY_DEATH_MESSAGES, "showNamedMobsOnly", false, "Only show death messages involving mobs which have a custom name. If MobvMob messages are enabled, the named mob can be the attacker as well.").getBoolean();
+        	showInanimateObjectOnMobDeathMessages = configuration.get(CATEGORY_DEATH_MESSAGES, "showInanimateObjectOnMobDeathMessages", true, "Show cases where a mob was killed by an inanimate object").getBoolean();
+        	showBatsBurningToDeath = configuration.get(CATEGORY_DEATH_MESSAGES, "showBatsBurningToDeath", false, "Show bats burning to death").getBoolean();
+
+        	xpForKillingSpree = configuration.get(CATEGORY_KILLING_SPREES, "xpForKillingSpree", 10, "Amount of XP awarded to players who get a killing spree.  Each subsequent level of spree gets twice as much xp.  Set to 0 to disable.").getInt();
+        	killingSpreePlayersEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreePlayersEnabled", true, "Show player killing sprees").getBoolean();
+        	killsForKillingSpree = configuration.get(CATEGORY_KILLING_SPREES, "killsPerBaseKillingSpree", 8, "Kills to get a level one killing spree.  Subsequent levels take twice as many per level.").getInt();
+        	
+        	//initialize the rest of the values using math
+        	killsForKillingMachine = (2 * killsForKillingSpree) + killsForKillingSpree;
+        	killsForLegendary = (4 * killsForKillingSpree) + killsForKillingMachine;
+        	killsForAwesome = (8 * killsForKillingSpree) + killsForLegendary;
+        	
+        	killingSpreePlayersVsMobsEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreePlayersVsMobsEnabled", true, "Show killing sprees when mobs kill players").getBoolean();
+        	killingSpreeMobsVsMobsEnabled = configuration.get(CATEGORY_KILLING_SPREES, "killingSpreeMobsVsMobsEnabled", true, "Show killing sprees when mobs kill mobs").getBoolean();
         }
         catch (Exception e)
         {
-            LogHelper.error(Reference.MOD_NAME + " has had a problem loading its client configuration");
+            LogHelper.error(Reference.MOD_NAME + " had a problem loading its client configuration");
         }
         finally
         {
